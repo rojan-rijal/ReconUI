3deea7c15fe7d0f73de3dbb523064f5a6df97fe0
Rojan Rijal <32774729+rojan-rijal@users.noreply.github.com>
2018-02-16 11:47:43 -0800

    Update installer.sh
    Added custom api insertion. AWS is still not added because it can be done with aws configure.

--- installer.sh
+++ installer.sh
@@ -20,3 +20,16 @@ sudo unzip /var/www.zip -d /var
 private_key=$(uuidgen)
 sudo sed  -i -e "s/PRIVATE_KEY/$private_key/g" /var/www/ManualScanner/app/home/views.py
 echo "Your private key is $private_key make sure to save it because you will need this to run the scan."
+echo "Now it is time for some custom setups"
+echo "Please copy & paste your Censys API ID. You can get this from: https://www.censys.io/account/api"
+read censys_app_id
+sudo sed  -i -e "s/CENSYS_UUID/$censys_app_id/g" /var/www/ManualScanner/app/home/helpers.py
+echo "Thank you. Now please copy paste your API KEY/Secret from the same link"
+read censys_secret
+sudo sed  -i -e "s/CENSYS_SECRET/$censys_secret/g" /var/www/ManualScanner/app/home/helpers.py
+echo "If you have setup your Heroku app to make it able to takeover domains, enter the app name below"
+read heroku_app_name
+sudo sed  -i -e "s/APP_NAME/$heroku_app_name/g" /var/www/ManualScanner/app/home/helpers.py
+echo "This app allows you take screenshot of each retrieved subdomains. However, to prevent massive server usage it uses URLScan.io. If you already requested an API key from them and have received one, paste it below"
+read urlscan_api_key
+sudo sed -i -e "s/URLSCAN_API/$urlscan_api_key/g" /var/www/ManualScanner/app/home/helpers.py
